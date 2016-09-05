af19f5a6e25602d43e050852fd8570cf93d77f28
Gabriel Appleby <gabrielappleby@Gabriels-MacBook.local>
2016-09-05 17:51:05 -0400

    Trying to get custom importer to work

---
+++ Assets/Scripts/Editor/CustomImporter_Tiles.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[Tiled2Unity.CustomTiledImporter]
+
+public class CustomImporter_Tiles : Tiled2Unity.ICustomTiledImporter
+{
+
+	public void HandleCustomProperties(GameObject gameObject,
+		IDictionary<string, string> customProperties)
+	{
+		FakeTransform test = gameObject.AddComponent<FakeTransform> ();
+		Object.Destroy (gameObject.GetComponent<EdgeCollider2D> ());
+	}
+
+	public void CustomizePrefab(GameObject prefab)
+	{
+		// Do nothing
+	}
+}
